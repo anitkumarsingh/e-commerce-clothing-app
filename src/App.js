081841cfec1ecfa0e8b772edb9be099671196815
anitkumarsingh <anikumarsingh99@gmail.com>
2022-11-27 12:26:57 +0530

    user auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React from 'react';
 import MainRouter from './Router';
+import { auth } from './Firebase/Firebase.utils';
 import './App.styles.scss';
 
-const App = () => {
-	return (
-		<>
-			<MainRouter />
-		</>
-	);
-};
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentUser: null
+		};
+	}
+	unSubcribeFromAuth = null;
+	componentDidMount() {
+		this.unSubcribeFromAuth = auth.onAuthStateChanged((user) => {
+			this.setState({ currentUser: user });
+		});
+	}
+	componentWillUnmount() {
+		this.unSubcribeFromAuth();
+	}
+
+	render() {
+		return <MainRouter />;
+	}
+}
 
 export default App;
