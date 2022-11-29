b47d8d8ff5c5f6d93ef23760cd23f3e78c7c7a09
anitkumarsingh <anikumarsingh99@gmail.com>
2022-11-29 15:51:24 +0530

    persist storage added

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -1,10 +1,18 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage  from 'redux-persist/lib/storage';
 import cartReducers from './Cart/cartReducer';
 import userReducer from './User/userReducer';
 
+const persistConfig ={
+  key:'root',
+  storage,
+  whitelist:['toggleCart']
+}
 
+const rootReducer = combineReducers({
+	user: userReducer,
+	toggleCart: cartReducers
+});
 
-export default combineReducers({
-  user:userReducer,
-  toggleCart:cartReducers
-})
+export default persistReducer(persistConfig,rootReducer)
--- src/Redux/Store/index.js
+++ src/Redux/Store/index.js
@@ -1,11 +1,12 @@
-import {createStore,applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 import rootReducer from '../Reducers';
-
 
 const middleware = [logger];
 
-const store = createStore(rootReducer,applyMiddleware(...middleware));
+export const store = createStore(rootReducer, applyMiddleware(...middleware));
+export const persistor = persistStore(store);
 
-
-export default store;
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { store, persistor };
