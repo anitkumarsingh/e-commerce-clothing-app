116e25b501ce591d78d3b83f61950a42b4ca9384
anitkumarsingh <anikumarsingh99@gmail.com>
2022-11-29 11:27:10 +0530

    checkout item page

---
+++ src/Components/Checkout-Item/Checkout-Item.component.jsx
@@ -0,0 +1,17 @@
+import './Checkout-Item-styles.scss';
+
+const CheckoutItem = ({cartItem:{name,quantity,price,imageUrl}}) => {
+	return (
+		<div className='checkout-item'>
+			<div className='image-container'>
+				<img src={imageUrl} alt='product-pic' />
+			</div>
+      <span className='name'>{name}</span>
+      <span className='quantity'>{quantity}</span>
+      <span className='price'>{price}</span>
+      <div className='remove-button'>&#10005;</div>
+		</div>
+	);
+};
+
+export default CheckoutItem;
--- src/Pages/Checkout/Checkout.component.jsx
+++ src/Pages/Checkout/Checkout.component.jsx
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 import './Checkout.styles.scss';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../Redux/Selectors/Cart';
+import CheckoutItem from '../../Components/Checkout-Item/Checkout-Item.component';
 
 const Checkout = ({ cartItems, total }) => {
 	return (
@@ -23,7 +24,9 @@ const Checkout = ({ cartItems, total }) => {
 					<span>Remove</span>
 				</div>
 			</div>
-      {cartItems.map((item) => item.name)}
+			{cartItems.map((item) => {
+				return(<CheckoutItem key={item.id} cartItem={item} />)
+			})}
 			<div className='total'>
 				<span>Total:${total}</span>
 			</div>
