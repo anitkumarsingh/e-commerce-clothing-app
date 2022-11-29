2ae15e93d5fa8408df2671072236c6ed3f88c0b7
anitkumarsingh <anikumarsingh99@gmail.com>
2022-11-29 10:00:46 +0530

    checkout markup and flow

--- src/Components/Cart-Dropdown/Cart-Dropdown.component.jsx
+++ src/Components/Cart-Dropdown/Cart-Dropdown.component.jsx
@@ -1,22 +1,23 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { selectCartItems } from '../../Redux/Selectors/Cart';
 import CartItems from '../Cart-Items/Cart-Items.component';
 import CustomButton from '../Custom-Button/Custom-Button.component';
 import './Cart-Dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => {
+const CartDropdown = ({ cartItems,history }) => {
 	return (
 		<div className='cart-dropdown'>
 			<div className='cart-items'>
-				{cartItems.map((item) => {
+	      {cartItems.length ? (cartItems.map((item) => {
 					return <CartItems items={item} key={item.id} />;
-				})}
+				})):(<span className='empty-message'>Your Cart Is Empty</span>)}
 			</div>
-			<CustomButton>CHECKOUT</CustomButton>
+			<CustomButton onClick={()=>history.push('/checkout')}>CHECKOUT</CustomButton>
 		</div>
 	);
 };
 const mapStateToProps = (state) => ({
 	cartItems: selectCartItems(state)
 });
-export default connect(mapStateToProps, null)(CartDropdown);
+export default withRouter(connect(mapStateToProps, null)(CartDropdown));
--- src/Router/index.js
+++ src/Router/index.js
@@ -2,6 +2,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from '../Components/Header/Header.component';
 import Homepage from '../Pages/Home/Homepage.component';
 import Shop from '../Pages/Shop/Shop.component';
+import Checkout from '../Pages/Checkout/Checkout.component';
 import SignInAndSignUP from '../Pages/Sign-in-and-Signup/Sign-in-and-Signup.component';
 import { connect } from 'react-redux';
 
@@ -12,6 +13,7 @@ const MainRouter = ({ currentUser }) => {
 			<Switch>
 				<Route exact path='/' component={Homepage} />
 				<Route path='/shop' component={Shop} />
+				<Route exact path='/checkout' component={Checkout} />
 				<Route
 					exact
 					path='/sign'
